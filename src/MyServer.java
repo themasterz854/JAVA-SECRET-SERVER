8436a245e02f2a2a7711f3447c51bcf9ded34ba8
themasterz854 <62380365+themasterz854@users.noreply.github.com>
2021-07-06 01:29:39 +0530

    somewhat secure login system implemented

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,5 +1,10 @@
 import java.net.*;
 import java.io.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+import java.util.Date;
+import static java.lang.Character.toLowerCase;
 
 class CustomSocket{
     Socket s;
@@ -8,48 +13,174 @@ class CustomSocket{
         id = -1;
     }
 }
-class Sender extends Thread{
-    Socket ReceiverSocket;
-    Socket SenderSocket;
+class Encryptor{
 
-    Sender(Socket ReceiverSocket,Socket SenderSocket)
-    {
-        this.SenderSocket= SenderSocket;
-        this.ReceiverSocket = ReceiverSocket;
+    String random1 = "!%*#(}]";
+    String random2 = "@$^&)[\\";
+    String random3 = "<>?:;_{|`";
+    Calendar c1 = Calendar.getInstance();
+    Date date = c1.getTime();
+    long time ;
+    char[] encrypteddata = new char[900];
 
-    }
-    public void run(){
-        String stro ;
-        DataOutputStream dout;
-        DataInputStream din;
-        System.out.println("ready to chat");
-        try {
-            dout = new DataOutputStream(ReceiverSocket.getOutputStream());
-            din = new DataInputStream(SenderSocket.getInputStream());
-            while(true) {
-                stro = din.readUTF();
-                if(stro.equals("exit"))
-                {
-                    dout = new DataOutputStream(SenderSocket.getOutputStream());
-                    dout.writeUTF("exit");
-                    dout.flush();
-                    dout.close();
-                    din.close();
-                    break;
-                }
-                dout.writeUTF(stro);
-                dout.flush();
-            }
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
+    public String encrypt(String data){
+      time = date.getTime();
+      int i,j=0,n,f;
+      char c,en = 'a';
+      n = data.length();
+      for(i =0;i<n;i++)
+      {
+          c = data.charAt(i);
+          if (c >= '0' && c <= '9')
+          {
+              switch (c)
+              {
+                  case '1': en = 'u';
+                      break;
+                  case '2': en = 'L';
+                      break;
+                  case '3': en = 'z';
+                      break;
+                  case '4': en = 'A';
+                      break;
+                  case '5': en = 'n';
+                      break;
+                  case '6': en = 'P';
+                      break;
+                  case '7': en = 's';
+                      break;
+                  case '8': en = 'G';
+                      break;
+                  case '9': en = 'w';
+                      break;
+                  case '0': en = 'I';
+                      break;
+              }
+          }
+          else if (c == ' ')
+          {
+              en = '\"';
+          }
+          else
+          {
+              if(c >= 'a' && c <= 'z')
+              {
+                  encrypteddata[j++] = random3.charAt((int) (time % 9));
+                  time++;
+              }
+              if(c >= 'a' && c <= 'z')
+              {
+                  f = Character.toUpperCase(c) - 16;
+              }
+              else
+              {
+                  f = c - 16;
+              }
+              if(f >= 58  && f <67)
+              {
+                  encrypteddata[j++] = random1.charAt((int) time % 7);
+                  en = (char)(f-9);
+                  time++;
+              }
+              else if(f >= 67 && f <=74)
+              {
+                  encrypteddata[j++] = random2.charAt((int)time %7);
+                  en = (char)(f - 18);
+              }
+              else
+                  en = (char)f;
+          }
+          encrypteddata[j++] = en;
+      }
+      String encryptedstr = new String(encrypteddata);
+      encryptedstr = encryptedstr.trim();
+      return encryptedstr;
     }
 }
-class Receiver extends Thread {
+class Decryptor{
+    public String decrypt(String data){
+        char[] decrypteddata = new char[300];
+        int i,j,n,flag ,f;
+        char c;
+        n = data.length();
+        j =0;
+        for(i=0;i<n;i++)
+        {
+            flag = 0;
+            c = data.charAt(i);
+            if(c == '\"')
+            {
+                decrypteddata[j++] = ' ';
+                continue;
+            }
+            if(c == '<' || c == '>' || c == '?' || c == ':' || c == ';' || c == '_' || c == '{' || c == '|' || c == '`')
+            {
+                flag = 1;
+                c = data.charAt(++i);
+            }
+            if(c == '@' || c == '$' || c == '^' || c == '&' || c == ')' || c == '[' || c == '\\')
+            {
+                c = data.charAt(++i);
+                f = c + 16;
+                c = (char)(f + 18);
+
+            }
+            else if(c == '!' || c == '#' || c == '%' || c == '*' || c == '(' || c == '}' || c == ']')
+            {
+                c = data.charAt(++i);
+                f = c + 16;
+                c = (char)(f+9);
+            }
+            else if(c >= '0' && c <= '9')
+            {
+                f = c +16;
+                c = (char)f;
+            }
+            else
+            {
+                switch (c)
+                {
+                    case 'u': c = '1';
+                        break;
+                    case 'L': c = '2';
+                        break;
+                    case 'z': c = '3';
+                        break;
+                    case 'A': c = '4';
+                        break;
+                    case 'n': c = '5';
+                        break;
+                    case 'P': c = '6';
+                        break;
+                    case 's': c = '7';
+                        break;
+                    case 'G': c = '8';
+                        break;
+                    case 'w': c = '9';
+                        break;
+                    case 'I': c = '0';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (flag == 1 && (c >= 'A' && c <= 'Z'))
+            {
+                c  = toLowerCase(c);
+            }
+            decrypteddata[j++] = c;
+        }
+        data = new String(decrypteddata);
+        data = data.trim();
+        return data;
+    }
+
+}
+class Manager extends Thread {
     CustomSocket sc;
     CustomSocket[] so;
 
-    Receiver(CustomSocket sc,int id,CustomSocket[] so){
+    Manager(CustomSocket sc, int id, CustomSocket[] so){
         this.sc = sc;
         this.so = so;
         this.sc.id = id;
@@ -134,27 +265,70 @@ class Receiver extends Thread {
 class Connector extends Thread{
     ServerSocket ss;
     CustomSocket[] so ;
-
-    Connector(ServerSocket ss,CustomSocket[] so)
-    {
+    File passfile = new File("C:\\Users\\Zaid\\Desktop\\uspass.txt");
+    Scanner filereader ;
+    FileWriter filewriter = new FileWriter(passfile,true);
+    String data;
+    String[] filedata;
+    String[] userdata;
+    DataOutputStream dout;
+    DataInputStream din;
+    Connector(ServerSocket ss,CustomSocket[] so) throws IOException {
         this.ss = ss;
         this.so = so;
     }
+
     public void run()
     {   int i =0;
-
+        Decryptor dec = new Decryptor();
+        Encryptor enc = new Encryptor();
+        int flag = 0;
+        String str,newusername,newpassword;
         while(true) {
 
             try {
-
                 so[i].s = ss.accept();
+                dout = new DataOutputStream(so[i].s.getOutputStream());
+                din = new DataInputStream(so[i].s.getInputStream());
+                str = din.readUTF();
+                if(str.equals("newaccount"))
+                {
+                    newusername = din.readUTF();
+                    newpassword = din.readUTF();
+
+                    filewriter.write(enc.encrypt(newusername+" "+newpassword)+"\n");
+                    filewriter.flush();
+                    filewriter.close();
+                }
+                else {
+                    userdata = str.split(" ");
+                    filereader = new Scanner(passfile);
+                    while (filereader.hasNextLine()) {
+                        data = filereader.nextLine();
+                        data = dec.decrypt(data);
+                        filedata = data.split(" ");
+                        if (filedata[0].equals(userdata[0]) && filedata[1].equals(userdata[1])) {
+                            dout.writeUTF("ok");
+                            dout.flush();
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if (flag == 1) {
+                        Manager res = new Manager(so[i], i, so);
+                        res.start();
+                        System.out.println("Client connected");
+                        i++;
+                    } else {
+                        dout.writeUTF("not ok");
+                        dout.flush();
+                    }
+                    flag = 0;
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Receiver res = new Receiver(so[i],i,so);
-            res.start();
-            System.out.println("Client connected");
-            i++;
+
         }
     }
 }
